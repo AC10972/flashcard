452dbb6fb7d47434951d28a9d11a1b6c44400092
AC10972 <anaclaramelo180@gmail.com>
2024-11-13 16:30:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo em extensão territorial?',
+    'Vaticano'
+)
+
+criaCartao(
+    'Charada',
+    'O que é que fica no meio do céu, mas nunca pode ser tocado?',
+    'A estrela'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o elemento químico com o símbolo O?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'Charada',
+    'O que é que nunca volta, mesmo que você o chame de novo?',
+    'O ontem'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Noite Estrelada?',
+    'Van Gogh'
+)
